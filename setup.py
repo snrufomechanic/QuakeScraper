2c2739bf32322b33e74bb1a77790324f13ea79f4
snrufomechanic <150843417+snrufomechanic@users.noreply.github.com>
2023-11-25 01:18:57 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-# setup.py
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='QuakeScraper',
-    version='0.1',
-    packages=['src'],
+    version='0.0.1',
+    packages=find_packages(),
     install_requires=[
         'requests',
         'beautifulsoup4',
-        'pandas'
+        'pandas',
     ],
 )
